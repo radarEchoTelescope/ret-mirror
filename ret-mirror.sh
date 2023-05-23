d3d972bcbb5d3aeeab0b122478387de4f661cd48
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2023-05-23 14:12:13 -0500

    modify a bit how we're mirroring things

--- ret-mirror.sh
+++ ret-mirror.sh
@@ -4,7 +4,7 @@ SLEEP_AMT=30
 DATA_DIR="/data" #this is on second disk
 DATA_DIR2="/data2" # this is on OS disk
 
-SOURCE="retsm@192.168.97.10:/data0"
+SOURCE="retsm@192.168.97.10:/data0/"
 
 RSYNC_OPTS="-avh -P -L" 
 
@@ -17,7 +17,7 @@ do
 
   if [ "$(stat -L -c '%d' $DATA_DIR)" = "$(stat -L -c '%d' $DATA_DIR2)" ]; then  
     echo "$DATA_DIR and $DATA_DIR2 are on same partition, copying only to $DATA_DIR2" 
-    rsync ${RSYNC_OPTS} $SOURCE $DATA_DIR2
+    rsync ${RSYNC_OPTS} $SOURCE $DATA_DIR
   else 
     rsync ${RSYNC_OPTS} $SOURCE $DATA_DIR
     rsync ${RSYNC_OPTS} $DATA_DIR $DATA_DIR2 
